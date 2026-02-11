046d126795ef8992835c80832ea2e65df5cc91f6
Gifton Njoroge <giftonnjoroge@gmail.com>
2026-02-11 18:42:47 +0300

    feat: add search view template and update index router with search controller

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const indexController = require("../controllers/indexCotroller");
+const indexController = require("../controllers/indexCotroller")
+const filter = require('../controllers/searchController')
 
 router.get("/", indexController.getIndexPage);
+// router.get('/', filter.filterItems)
 
 module.exports = router;
