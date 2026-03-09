5d3a4573e914262738ccae074af6544c75ec89c3
Gifton Njoroge <giftonnjoroge@gmail.com>
2026-03-09 20:33:31 +0300

    Refactor viewMyPosts function by adding try catch for error handling

--- controllers/myPostsController.js
+++ controllers/myPostsController.js
@@ -3,19 +3,23 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 exports.viewMyPosts = async (req, res) => {
-    const token = req.cookies.accessToken
-    const accessToken = process.env.ACCESS_TOKEN
+    try{
+        const token = req.cookies.accessToken
+        const accessToken = process.env.ACCESS_TOKEN
 
-    const userPayload = jwt.verify(token, accessToken)
+        const userPayload = jwt.verify(token, accessToken)
 
-    const findUser = await User.findById(userPayload.userId)
+        const findUser = await User.findById(userPayload.userId)
 
-    const findPosts = await Post.where("user").equals(findUser)
+        const findPosts = await Post.where("user").equals(findUser)
 
-    console.log("findUser: ", findUser)
-    console.log("findpost: ", findPosts)
-
-    res.render("myPostView/myPosts", {
-        post: findPosts
-    })
+        res.render("myPostView/myPosts", {
+            post: findPosts
+        })
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).send("Server Error")
+    }
+    
 }
