fe711847c2f579da81d1a5d53b4359d0736752ca
Gifton Njoroge <giftonnjoroge@gmail.com>
2026-03-10 14:38:37 +0300

    Implement edit functionality on my posts

--- controllers/myPostsController.js
+++ controllers/myPostsController.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post')
 const User = require('../models/user')
+const Category = require("../models/category")
 const jwt = require('jsonwebtoken')
 
 exports.viewMyPosts = async (req, res) => {
@@ -22,4 +23,33 @@ exports.viewMyPosts = async (req, res) => {
         res.status(500).send("Server Error")
     }
     
+}
+
+exports.editMyPost = async (req, res) => {
+    try {
+        const postId = req.params.id
+        const edit = await Post.findById(postId)
+        const categories = await Category.find({})
+
+        console.log("edit: ", edit)
+        res.render("post/edit", {
+            post: new Post(edit),
+            categories: categories
+        })
+    }
+    catch(err) {
+        res.redirect('myPost/:id')
+    }
+    
+}
+
+exports.updatePost = async (req, res) => {
+    const postId = req.params.id
+    console.log("PsotID: ", postId)
+
+    const edited = await Post.findByIdAndUpdate(postId, req.body, { new: true })
+
+    console.log("Edited: ", edited)
+
+    res.redirect('/myPost')
 }
--- routers/myPost.js
+++ routers/myPost.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const myPost = require('../controllers/myPostsController')
 
 router.get('/', myPost.viewMyPosts)
+router.get('/:id', myPost.editMyPost)
+router.post('/:id', myPost.updatePost)
 
 module.exports = router
